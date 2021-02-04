5243be49594542262db7897afba7b58c278bb238
OmegaWeaponDev <omegaweapondev@outlook.com.au>
2021-02-04 20:45:20 +1030

    Stylizer - Make name colour command global

--- src/main/java/me/omegaweapondev/stylizer/commands/NameColour.java
+++ src/main/java/me/omegaweapondev/stylizer/commands/NameColour.java
@@ -3,12 +3,15 @@ package me.omegaweapondev.stylizer.commands;
 import me.omegaweapondev.stylizer.Stylizer;
 import me.omegaweapondev.stylizer.utilities.MessageHandler;
 import me.ou.library.Utilities;
-import me.ou.library.commands.PlayerCommand;
+import me.ou.library.commands.GlobalCommand;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-public class NameColour extends PlayerCommand {
+import javax.annotation.Nullable;
+
+public class NameColour extends GlobalCommand {
   private final Stylizer plugin;
 
   private final MessageHandler messageHandler;
@@ -23,28 +26,39 @@ public class NameColour extends PlayerCommand {
   }
 
   @Override
-  protected void execute(final Player player, final String[] strings) {
+  protected void execute(final CommandSender commandSender, final String[] strings) {
 
-    if(strings.length == 0) {
-      openNameColourGUI(player);
-      return;
-    }
+    if(commandSender instanceof Player) {
+      final Player player = ((Player) commandSender).getPlayer();
 
-    if(strings.length == 2) {
-      switch(strings[0]) {
-        case "remove":
-          removeNameColour(player, strings);
-          break;
-        case "custom":
-          customNameColour(player, strings);
-          break;
-        default:
-          break;
+      if(strings.length == 0) {
+        openNameColourGUI(player);
+        return;
+      }
+
+      if(strings.length == 2) {
+        removeNameColour(player, strings);
+        return;
+      }
+
+      if(strings.length == 3) {
+        addNameColour(player, strings);
+        return;
       }
     }
 
+    if(strings.length == 0) {
+      Utilities.logInfo(true, "You must be a player to open the NameColour GUI");
+      return;
+    }
+
+    if(strings.length == 2 && strings[0].equalsIgnoreCase("remove")) {
+      removeNameColour(null, strings);
+      return;
+    }
+
     if(strings.length == 3) {
-      addNameColour(player, strings);
+      addNameColour(null, strings);
     }
   }
 
@@ -57,7 +71,7 @@ public class NameColour extends PlayerCommand {
     plugin.getNameColourGUI().openInventory(player);
   }
 
-  private void addNameColour(final Player player, final String[] strings) {
+  private void addNameColour(@Nullable final Player player, final String[] strings) {
     final Player target = Bukkit.getPlayer(strings[1]);
     final String nameColour = strings[2];
 
@@ -65,13 +79,19 @@ public class NameColour extends PlayerCommand {
       return;
     }
 
-    if(!Utilities.checkPermissions(player, true, "stylizer.namecolour.add", "stylizer.admin")) {
-      Utilities.message(player, messageHandler.string("No_Permission", "&cSorry, you do not have permission to do that."));
-      return;
+    if(player != null) {
+      if(!Utilities.checkPermissions(player, true, "stylizer.namecolour.add", "stylizer.admin")) {
+        Utilities.message(player, messageHandler.string("No_Permission", "&cSorry, you do not have permission to do that."));
+        return;
+      }
     }
 
     if(target == null) {
-      Utilities.message(player, messageHandler.string("Invalid_Player", "&cSorry, that player cannot be found."));
+      if(player != null) {
+        Utilities.message(player, messageHandler.string("Invalid_Player", "&cSorry, that player cannot be found."));
+        return;
+      }
+      Utilities.logInfo(true, "Sorry, that player cannot be found.");
       return;
     }
 
@@ -86,60 +106,43 @@ public class NameColour extends PlayerCommand {
     Utilities.message(target, messageHandler.string("Name_Colour_Applied", "&bYour name colour has been changed to: %namecolour%").replace("%namecolour%", nameColour + player.getName()));
   }
 
-  private void removeNameColour(final Player player, final String[] strings) {
+  private void removeNameColour(@Nullable final Player player, final String[] strings) {
     final Player target = Bukkit.getPlayer(strings[1]);
 
     if(!strings[0].equalsIgnoreCase("remove")) {
       return;
     }
 
-    if(!Utilities.checkPermissions(player, true, "stylizer.namecolour.remove", "stylizer.admin")) {
-      Utilities.message(player, messageHandler.string("No_Permission", "&cSorry, you do not have permission to do that."));
-      return;
+    if(player != null) {
+      if(!Utilities.checkPermissions(player, true, "stylizer.namecolour.remove", "stylizer.admin")) {
+        Utilities.message(player, messageHandler.string("No_Permission", "&cSorry, you do not have permission to do that."));
+        return;
+      }
     }
 
     if(target == null) {
-      Utilities.message(player, messageHandler.string("Invalid_Player", "&cSorry, that player cannot be found."));
+      if(player != null) {
+        Utilities.message(player, messageHandler.string("Invalid_Player", "&cSorry, that player cannot be found."));
+        return;
+      }
+      Utilities.logInfo(true, "Sorry, that player cannot be found.");
       return;
     }
 
     for(String groupName : configFile.getConfigurationSection("Group_Name_Colour.Groups").getKeys(false)) {
 
-      if(Utilities.checkPermission(player, false, "stylizer.namecolour.groups." + groupName)) {
-        player.setDisplayName(Utilities.colourise(configFile.getString("Group_Name_Colour.Groups." + groupName) + player.getName()));
-        playerData.set(player.getUniqueId().toString() + ".Name_Colour", plugin.getConfigFile().getConfig().getString("Group_Name_Colour.Groups." + groupName));
+      if(Utilities.checkPermission(target, false, "stylizer.namecolour.groups." + groupName)) {
+        target.setDisplayName(Utilities.colourise(configFile.getString("Group_Name_Colour.Groups." + groupName) + target.getName()));
+        playerData.set(target.getUniqueId().toString() + ".Name_Colour", plugin.getConfigFile().getConfig().getString("Group_Name_Colour.Groups." + groupName));
         plugin.getPlayerData().saveConfig();
         Utilities.message(target, messageHandler.string("Name_Colour_Removed", "&cYour name colour has been reverted to the default colour"));
         return;
       }
 
-      player.setDisplayName(Utilities.colourise(configFile.getString("Default_Name_Colour", "&e") + player.getName()));
-      playerData.set(player.getUniqueId().toString() + ".Name_Colour", configFile.getString("Default_Name_Colour", "&e"));
+      target.setDisplayName(Utilities.colourise(configFile.getString("Default_Name_Colour", "&e") + target.getName()));
+      playerData.set(target.getUniqueId().toString() + ".Name_Colour", configFile.getString("Default_Name_Colour", "&e"));
       plugin.getPlayerData().saveConfig();
       Utilities.message(target, messageHandler.string("Name_Colour_Removed", "&cYour name colour has been reverted to the default colour"));
     }
-  }
-
-  private void customNameColour(final Player player, final String[] strings) {
-
-    if(!strings[0].equalsIgnoreCase("custom")) {
-      return;
-    }
-
-    if(!Utilities.checkPermissions(player, true, "stylizer.namecolour.custom", "stylizer.admin")) {
-      Utilities.message(player, messageHandler.string("No_Permission", "&cSorry, you do not have permission to do that."));
-      return;
-    }
-
-    final String customColour = strings[1];
-
-    if(strings[1].equalsIgnoreCase("")) {
-      return;
-    }
-
-    player.setDisplayName(Utilities.colourise(customColour));
-    playerData.set(player.getUniqueId().toString() + ".Name_Colour", customColour);
-    plugin.getPlayerData().saveConfig();
-    Utilities.message(player, "Name_Colour_Applied".replace("%namecolour%", player.getDisplayName()));
   }
 }
